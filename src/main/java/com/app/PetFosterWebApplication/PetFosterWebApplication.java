78f5182d248acdb9d27229bebaee2b38f3eca7b8
swapsgit <swapnilsabswapnil@gmail.com>
2025-01-02 16:26:42 +0530

    cdac projx
    this is frontend react and backend springboot

---
+++ src/main/java/com/app/PetFosterWebApplication/PetFosterWebApplication.java
@@ -0,0 +1,13 @@
+package com.app.PetFosterWebApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetFosterWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetFosterWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/PetFosterWebApplication/PetFosterWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.PetFosterWebApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetFosterWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
